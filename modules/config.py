ecf64dc15b07ba73f3e4177330cadfd59e8ea15a
aimlesslyw4lking <dddavtian@edu.hse.ru>
2026-03-03 21:10:55 +0300

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -7,25 +7,16 @@ env.read_env()
 def load_RNC_token() -> str:
     return env.str("RNC_TOKEN")
 
-def load_BNC_token() -> str:
-    return env.str("BNC_TOKEN")
-
-def load_COCA_token() -> str:
-    return env.str("COCA_TOKEN")
-
 @dataclass
 class CorpusSettings:
-    COCA_SUBCORPUS: str
-    BNC_SUBCORPUS: str
     RNC_SUBCORPUS: str
     DATE_START: int
     DATE_END: int
 
 def load_corpus_settings() -> CorpusSettings:
     return CorpusSettings(
-        COCA_SUBCORPUS=env.str("COCA_SUBCORPUS", default="MAIN"),
-        BNC_SUBCORPUS=env.str("BNC_SUBCORPUS", default="MAIN"),
         RNC_SUBCORPUS=env.str("RNC_SUBCORPUS", default="MAIN"),
         DATE_START=env.int("DATE_START", default=2000),
         DATE_END=env.int("DATE_END", default=2026)
     )
+    
